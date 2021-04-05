65407cde3de009114dcaf93ecfe7479788b41210
VanMachado <vandamme.machado@gmail.com>
2021-04-05 20:51:58 -0300

    Treinando Caelum

---
+++ src/br/com/van/machado/caelum/cap3/Main.java
@@ -0,0 +1,18 @@
+package br.com.van.machado.caelum.cap3;
+
+public class Main {
+
+	/* Q1) Imprima todos os números de 150 a 300. */
+
+	public static void questao01() {
+
+		for (int i = 150; i <= 300; i++) {
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		questao01();
+	}
+
+}
